01c659690e75ea51e50214560410ef24f335b865
Samantha Ferri <samantha.manuela@hotmail.com>
2023-05-29 20:55:11 -0300

    fetch button

--- script/main.js
+++ script/main.js
@@ -1,5 +1,11 @@
-var input = document.Calculator.text1;
+var input = Calculator.text1;
 
+//Funcionalidade da calculadora
+function displaynum(n1) {
+  input.value = input.value + n1;
+}
+
+//Função na tecla ENTER
 input.addEventListener("keyup", function(event) {
   if (event.keyCode === 13) {
     calculate()
@@ -8,10 +14,22 @@ input.addEventListener("keyup", function(event) {
   }
 });
 
-function displaynum(n1) {
-    input.value = input.value + n1;
-  }
-
 function calculate() {
     input.value = eval(input.value);
   }
+
+  //Identificação do usuário por requisição assíncrona
+  var fetchButton = getElementById("fetchButton");
+  var identificationElement = getElementById("identification");
+
+  fetchButton.addEventListener("click", function() {
+    fetch("/identificacao.json")
+      .then(response => response.json())
+      .then(data => {
+        var identification = data.identification;
+        identificationElement.textContent = "Sua identificação é: " + identification;
+      })
+      .catch(error => {
+        console.log("Ocorreu um erro na requisição: ", error);
+      });
+  });
