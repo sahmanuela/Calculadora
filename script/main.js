1cd463d51bb37db4ee7828dc38e90c855552deca
Samantha Ferri <samantha.manuela@hotmail.com>
2023-05-29 19:23:55 -0300

    funcionalidade tecla enter

--- script/main.js
+++ script/main.js
@@ -1,3 +1,17 @@
+var input = document.Calculator.text1;
+
+input.addEventListener("keyup", function(event) {
+  if (event.keyCode === 13) {
+    calculate()
+  } else {
+    displaynum(event.key);
+  }
+});
+
 function displaynum(n1) {
-    Calculator.text1.value = Calculator.text1.value + n1;
-}
+    input.value = input.value + n1;
+  }
+
+function calculate() {
+    input.value = eval(input.value);
+  }
